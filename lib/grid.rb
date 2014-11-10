cafc24b12ed3cb37a713be33e0ea04603208c743
Nadav <nadav.matalon@gmail.com>
2014-11-10 23:05:26 +0000

    code refactoring

--- lib/grid.rb
+++ lib/grid.rb
@@ -2,14 +2,7 @@ require './lib/square.rb'
 
 class Grid
 
-	NUMBER_OF_COLUMNS = 9
-	NUMBER_OF_ROWS = 9
-	NUMBER_OF_BOXES = 9
 	NUMBER_OF_SQUARES = 81
-
-	EASY_PUZZLE = '015003002000100906270068430490002017501040380003905000900081040860070025037204600'
-	MEDIUM_PUZZLE = '000200001060075000057004060900000608000080000005630040500003000002000930708000014'
-	HARD_PUZZLE = '800000000003600000070090200050007000000045700000100030001000068008500010090000400'
 
 	def initialize (puzzle = nil)
 		generate_squares
@@ -64,16 +57,21 @@ class Grid
   		squares.count(&:unsolved?) == 0
   	end
 
-	def solve 
-		start_time, current_state, stop_looping = Time.now, NUMBER_OF_SQUARES, false
+	def solve
+		start_time = Time.now
+		first_attempt start_time
+		try_again unless fully_solved? || too_much_time_since(start_time, 1.50)
+		fully_solved? ? 'SOLVED IT!' : 'COULDN\'T SOLVE IT!'
+	end
+
+	def first_attempt start_time
+		current_state, stop_looping = NUMBER_OF_SQUARES, false
 		while !fully_solved? && !stop_looping
 			squares.each { |square| solve_square_in square.index }
 			new_state = squares.count(&:solved?)
 			stop_looping = true if current_state == new_state || too_much_time_since(start_time, 0.75)
 			current_state = new_state
 		end
-		try_again unless fully_solved? || too_much_time_since(start_time, 1.50)
-		fully_solved? ? 'SOLVED IT!' : 'COULDN\'T SOLVE IT!'
 	end
 
 	def too_much_time_since start_time, interval_in_seconds
@@ -97,23 +95,15 @@ class Grid
 		candidates = candidates_for empty_square.index
 		candidates.each do |candidate|
 			empty_square.set candidate
-			grid = duplicate
+			grid = self.class.new(self.current_state)
 			grid.solve
-			upload_solution_to grid and return if grid.fully_solved?
+			upload grid.current_state and return if grid.fully_solved?
 		end
-	end
-
-	def duplicate
-		self.class.new(self.current_state)
 	end
 
   	def current_state 
   		squares.map {|square| square.value}.join
   	end
-
-	def upload_solution_to grid
-		upload grid.current_state
-	end
 
 	def print_in_terminal
  		puts grid_str_for_print
