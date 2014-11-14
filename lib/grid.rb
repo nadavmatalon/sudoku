6e50a9a5c36853a85e162b569d8a6354e993d94e
Nadav <nadav.matalon@gmail.com>
2014-11-14 15:15:57 +0000

    complete rewerite of the grid class and specs

--- lib/grid.rb
+++ lib/grid.rb
@@ -1,123 +1,107 @@
-require './lib/square.rb'
-
 class Grid
 
-	NUMBER_OF_SQUARES = 81
+	attr_accessor :puzzle
 
-	def initialize (puzzle = nil)
-		generate_squares
-		upload puzzle if puzzle
-	end
- 
-	def squares 
-		@squares ||= []
+	def initialize (puzzle = '0' * 81)
+		upload puzzle
 	end
 
-	def generate_squares
-		row, column, box, value = 1, 1, 1, 0
-		for index in 1..NUMBER_OF_SQUARES do
-			squares << Square.new(index - 1, row, column, box, value)
-			column < 9 ? column += 1 : (column = 1 ; row += 1)
-			box = set_box_value(index, row, column, box)
+	def upload puzzle
+		@puzzle = puzzle.chars.map(&:to_i)
+	end
+
+	def get_value_at index
+		puzzle[index]
+	end
+
+	def set_value_at index, value
+		puzzle[index] = value
+	end
+
+	def puzzle_to_str
+		@puzzle.join
+	end
+
+	def solved_at? index
+		get_value_at(index) != 0
+	end
+
+	def fully_solved?
+		return true unless puzzle.include? 0
+	end
+
+	def puzzle_rows
+		puzzle.each_slice(9).to_a
+	end
+
+	def puzzle_columns
+		puzzle_rows.transpose
+	end
+
+	def puzzle_boxes
+		(0..2).reduce([]) do |boxes, index|
+			selected_rows = puzzle_rows.slice(3 * index, 3)
+			boxes + selected_rows.transpose.each_slice(3).map(&:flatten)
 		end
-	end 
-
- 	def set_box_value index, row, column, box
-		box += 1 if ((column - 1) % 3) == 0
-		box = 1 if index % 9 == 0 && row <= 3
-		box = 4 if index % 9 == 0 && row >= 4 && row <= 6
-		box = 7 if index % 9 == 0 && row >= 7
-		box
 	end
 
-  	def upload puzzle
-		squares.each_with_index { |square, index| square.value = puzzle.chars[index].to_i }
-  	end	
-
-	def peers_for selected_square
-		row, column, box = selected_square.row, selected_square.column, selected_square.box
-		peers = squares.select { |square| square if square.row == row || square.column == column || square.box == box }
- 	 	peers - [selected_square]
+	def peer_values_of index
+		peers = puzzle_rows[index/9] + puzzle_columns[index%9] + puzzle_boxes[find_box_of(index)]
+		peers.flatten.sort.uniq.reject { |value| value == (0 or value_of(index)) }
 	end
 
-	def values_of peers
-		peers.map { |peer| peer.value unless peer.value == 0 }.compact.uniq.sort
+	def find_box_of index
+		new_grid, new_grid.puzzle = self.clone, Array.new(81) { |index| index }
+		new_grid.puzzle_boxes.map { |boxes| boxes & [index] }.map.with_index { |box ,index| index unless box.empty? }.compact[0]
 	end
 
 	def candidates_for index
-		candidates = (1..9).to_a - values_of(peers_for squares[index])
-		unsolved?(index) ? candidates.sort : []
+		candidates = (1..9).to_a - peer_values_of(index)
+		candidates.sort if !solved_at?(index)
 	end
 
-	def unsolved? index
-		squares[index].value == 0
+	def solve_at index
+		solutions = candidates_for(index) || []
+		set_value_at(index, solutions.first) if solutions.count == 1
 	end
-
-  	def fully_solved?
-  		squares.count(&:unsolved?) == 0
-  	end
 
 	def solve
-		start_time = Time.now
-		first_attempt start_time
-		try_again unless fully_solved? || too_much_time_since(start_time, 1.50)
-		fully_solved? ? 'SOLVED IT!' : 'COULDN\'T SOLVE IT!'
+		first_attempt_to_solve
+		second_attempt_to_solve unless fully_solved?
+		fully_solved?
 	end
 
-	def first_attempt start_time
-		current_state, stop_looping = NUMBER_OF_SQUARES, false
+	def first_attempt_to_solve
+		current_puzzle_state, stop_looping = 81, false
 		while !fully_solved? && !stop_looping
-			squares.each { |square| solve_square_in square.index }
-			new_state = squares.count(&:solved?)
-			stop_looping = true if current_state == new_state || too_much_time_since(start_time, 0.75)
-			current_state = new_state
+			new_puzzle_state = solve_all_squares
+			stop_looping = true if current_puzzle_state == new_puzzle_state
+			current_puzzle_state = new_puzzle_state
 		end
 	end
 
-	def too_much_time_since start_time, interval_in_seconds
-		Time.now - start_time > interval_in_seconds
-	end
-
-  	def solve_square_in index
-  		if unsolved?(index) && candidates_for(index).count == 1
-  			squares[index].value = candidates_for(index).first
-  		end
-	end
-
-	def try_again
-		empty_square = squares.reject(&:solved?).first
- 		candidates_for(empty_square.index).each do |candidate|
-			empty_square.set candidate
-			grid = self.class.new(self.current_state)
-			upload grid.current_state and return if grid.solve == 'SOLVED IT!'
+	def second_attempt_to_solve
+		empty_index = find_first_empty_index
+ 		candidates_for(empty_index).each do |candidate|
+			set_value_at empty_index, candidate
+			grid = self.class.new(self.puzzle_to_str)
+			upload grid.puzzle_to_str and return if grid.solve
 		end
 	end
 
-  	def current_state
-  		squares.map { |square| square.value }.join
-  	end
+	def solve_all_squares
+		puzzle.each_with_index { |square, index| solve_at(index) }
+		puzzle.count { |value| value != 0 }
+	end
 
-	def print_in_terminal
- 		puts grid_str_for_print
-	end	
+	def find_first_empty_index
+		puzzle.map.with_index { |value, index| index if value == 0 }.compact.first
+	end
 
-	def grid_str_for_print
-	 	rows = current_state.chars.each_slice(9).map do |row|
-	 		row.insert(3, '|').insert(7, '|').insert(11, "\n").join(' ')
-	 	end
+	def str_for_print
 		separator = '-' * 21 + "\n"
-		rows.insert(3, separator).insert(7, separator).join(' ').insert(0, ' ')
+		rows = puzzle_rows.each { |row| row.insert(3, '|').insert(7, '|').insert(11, "\n").join(' ') }
+		rows.insert(3, separator).insert(7, separator).join(' ').prepend("\n ").concat("\n")
 	end
-
-	def map_random_values_to box_number
-		random_values = (1..9).sort_by { rand }
-		selected_squares = find_square_indexes_of box_number
-		selected_squares.each_with_index { |square, index| squares[square].value = random_values[index] }
-	end
-
-	def find_square_indexes_of box_number
-		squares.map { |square| square.index if square.box == box_number }.compact
-	end
-
 end
 
--- spec/grid_spec.rb
+++ spec/grid_spec.rb
@@ -1,160 +1,120 @@
+
 describe Grid do
 
-    let (:grid) { Grid.new }
+    let (:puzzle) { '786214590150369080003708416267093800490800601308500072072400300641035700000602054' }
+    let (:very_easy_puzzle) { '009006080000457210021389465150802937090003801873005604980534006400701398300068540' }
+    let (:easy_puzzle) { '630100009700030600490607130050300090947865000103000400829000570060798301071056000' }
+    let (:medium_puzzle) { '810000000000100304050706018140300062005009800000000000020900405093810700406500000' }
+    let (:hard_puzzle) { '008003000301000200029005000200070000000506400100040008000020000000090100000000800' }
+    let (:very_hard_puzzle) { '000000700000000600300000100000006000000010000000070001000060500000000000000000000' }
+    let (:impossible_puzzle) { '516849732307605000809700065135060907472591006968370050253186074684207500791050608' }
+    let (:grid) { Grid.new puzzle }
 
-    it "should have 81 squares" do
-        expect(grid.squares.count).to eq 81
+    it 'is initialized with empty puzzle by defualt' do
+        expect(Grid.new.puzzle).to eq Array.new(81, 0)
     end
 
-    it "can be initialized without a puzzle (defualt)" do
-        grid.squares.each do |square|
-            expect(square.value).to eq 0
-        end
+    it 'can be initiallized with a puzzle' do
+        expect(grid.puzzle.join).to eq puzzle
     end
 
-    it "can be initiallized with a puzzle" do
-        puzzle = "786214590150369080003708416267093800490800601308500072072400300641035700000602054"
-        grid = Grid.new puzzle
-        grid.squares.each_with_index do |square, index| 
-        expect(square.value).to eq puzzle.chars[index].to_i
-        end
-    end
-
-    it "can upload a puzzle after initialization" do
-        puzzle = "786214590150369080003708416267093800490800601308500072072400300641035700000602054"
+    it 'can have puzzle uploaded after initialization' do
+        grid = Grid.new
         grid.upload puzzle
-        grid.squares.each_with_index do |square, index| 
-            expect(square.value).to eq puzzle.chars[index].to_i
-        end
+        expect(grid.puzzle.join).to eq puzzle
     end
 
-    it "can find the peers of a square" do
-        index_of_peers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 18, 19, 20, 27, 36, 45, 54, 63, 72]
-        peers = grid.peers_for grid.squares[0]
-        peers.each_with_index do |peer, index|
-            expect(peer.index).to eq index_of_peers[index]
-        end
+    it 'can get the value of a square based on it\'s index' do
+        expect(grid.get_value_at 0).to eq 7
     end
 
-    it "can find the values of the peers of a square" do
-        puzzle = '015003002000100906270068430490002017501040380003905000900081040860070025037204600'
-        grid = Grid.new puzzle
-        peers_values = [1, 2, 3, 4, 5, 7, 8, 9]
-        peers = grid.peers_for grid.squares[0]
-        expect(grid.values_of peers).to eq peers_values
+    it 'can set the value of a square based on it\'s index' do
+        grid.set_value_at 0, 2
+        expect(grid.get_value_at 0).to eq 2
     end
 
-    it "can find solution candidates for a square" do
-        puzzle = '015003002000100906270068430490002017501040380003905000900081040860070025037204600'
-        grid = Grid.new puzzle
-        expect(grid.candidates_for 0).to eq [6]
+    it 'can exort it\'s puzzle current state to a str' do
+        expect(grid.puzzle_to_str).to eq puzzle
     end
 
-    it "knows if a square is unsolved" do
-        expect(grid.unsolved? 0).to be true
-        grid.squares[0].value = 1
-        expect(grid.unsolved? 0).to be false
+    it 'knows if a square is solved based on it\'s index' do
+        expect(grid.solved_at? 0).to be true
     end
 
-    it "can solve a square" do
-        puzzle = '015003002000100906270068430490002017501040380003905000900081040860070025037204600'
-        grid = Grid.new puzzle
-        grid.solve_square_in 0
-        expect(grid.squares[0].value).to eq 6
+    it 'knows if a square is not solved based on it\'s index' do
+        expect(grid.solved_at? 8).to be false
     end
 
-    it "skips over a square if it\'s already solved" do
-        puzzle = '015003002000100906270068430490002017501040380003905000900081040860070025037204600'
-        grid = Grid.new puzzle
-        expect(grid.solve_square_in 1).to eq nil
+    it 'can find the box number of a square based on it\'s index' do
+        [10, 13, 16].each_with_index { |value, index| expect(grid.find_box_of value).to eq index }
+        [37, 40, 43].each_with_index { |value, index| expect(grid.find_box_of value).to eq index+3 }
+        [64, 67, 70].each_with_index { |value, index| expect(grid.find_box_of value).to eq index+6 }
     end
 
-    it "knows it\'s current state" do
-        puzzle = '0' * 81
-        grid = Grid.new puzzle
-        expect(grid.current_state).to eq puzzle
-        puzzle = '015003002000100906270068430490002017501040380003905000900081040860070025037204600'
-        grid = Grid.new puzzle
-        expect(grid.current_state).to eq puzzle
+    it 'can find all peer values of a square at a given index' do
+        expect(grid.peer_values_of 40).to eq [1, 3, 4, 5, 6, 8, 9]
     end
 
-    it "knows if it\'s still unsolved" do
-        grid = Grid.new
-        expect(grid.fully_solved?).to be false
+    it 'can find solution candidates for a square at a given index' do
+        expect(grid.candidates_for 40).to eq [2, 7]
     end
 
-    it "knows if it\'s fully solved" do
-        puzzle = '015003002000100906270068430490002017501040380003905000900081040860070025037204600'
-        grid = Grid.new puzzle
+    it 'solution candidates search returns \'nil\' for solved squares' do
+        expect(grid.candidates_for 0).to eq nil
+    end
+
+    it 'can solve an unsolved square at a given index' do
+        expect(grid.solve_at 11).to eq 4
+    end
+
+    it 'should not solve an already solved square' do
+        expect(grid.solve_at 0).to be_falsey
+    end
+
+    it 'should not solve an unsolved square with more than one solution candidate' do
+        expect(grid.solve_at 40).to be_falsey
+    end
+
+    it 'knows if it\'s not fully solved' do
+        expect(grid.fully_solved?).to be_falsey
+    end
+
+    it 'knows if it\'s fully solved' do
         grid.solve
         expect(grid.fully_solved?).to be true
     end
 
-    it "can solve an empty puzzle" do
-        grid = Grid.new
-        grid.solve
-        expect(grid.fully_solved?).to be true
+    it 'can solve an empty puzzle' do
+        expect(Grid.new.solve).to be true
     end
 
-    it "can solve a very easy puzzle" do
-        very_easy_puzzle = "786214590150369080003708416267093800490800601308500072072400300641035700000602054"
-        grid = Grid.new very_easy_puzzle
-        grid.solve
-        expect(grid.fully_solved?).to be true
+    it 'can solve a very easy puzzle' do
+        expect(Grid.new(very_easy_puzzle).solve).to eq true
     end
 
-    it "can solve an easy puzzle" do
-        easy_puzzle = "520130000000062005046500109000070984280301650009804000051923000473600090002745060"
-        grid = Grid.new easy_puzzle
-        grid.solve
-        expect(grid.fully_solved?).to be true
+    it 'can solve an easy puzzle' do
+        expect(Grid.new(easy_puzzle).solve).to eq true
     end
 
-    it "can solve a medium puzzle" do
-        medium_puzzle = "003510000010780090700246000004060800000058930000000000002305009956020000300600208"
-        grid = Grid.new medium_puzzle
-        grid.solve
-        expect(grid.fully_solved?).to be true
+    it 'can solve a medium puzzle' do
+        expect(Grid.new(medium_puzzle).solve).to eq true
     end
 
-    it "can solve a hard puzzle" do
-        hard_puzzle = "800003000000006001000400050003004800006100000000000500000000005000630700050821000"
-        grid = Grid.new hard_puzzle
-        grid.solve
-        expect(grid.fully_solved?).to be true
+    it 'can solve a hard puzzle' do
+        expect(Grid.new(hard_puzzle).solve).to eq true
     end
 
-    it "can solve a very hard puzzle" do
-        very_hard_puzzle = "600040000041000000009000000000071090000000000000080000000000200000000000000000000"
-        grid = Grid.new very_hard_puzzle
-        grid.solve
-        expect(grid.fully_solved?).to be true
+    it 'can solve a very hard puzzle' do
+        expect(Grid.new(very_hard_puzzle).solve).to eq true
     end
 
-    it "gives correct feedback after solving a puzzle" do
-        puzzle = "786214590150369080003708416267093800490800601308500072072400300641035700000602054"
-        grid = Grid.new puzzle
-        expect(grid.solve).to eq "SOLVED IT!"
+    it 'cannot solve an impossible puzzle' do
+        expect(Grid.new(impossible_puzzle).solve).to be_falsey
     end
 
-    it "knows if it can't solve puzzle" do
-        impossible_puzzle = '516849732307605000809700065135060907472591006968370050253186074684207500791050608'
-        grid = Grid.new impossible_puzzle
-        grid.solve
-        expect(grid.fully_solved?).to be false
+    it 'can print the grid\'s current state in terminal' do
+        puts grid.str_for_print
     end
 
-    it "gives correct feedback after solving a puzzle" do
-        impossible_puzzle = '516849732307605000809700065135060907472591006968370050253186074684207500791050608'
-        grid = Grid.new impossible_puzzle
-        expect(grid.solve).to eq "COULDN'T SOLVE IT!"
-    end
-
-    it "can print the current state of a grid in the terminal" do
-        puzzle = "786214590150369080003708416267093800490800601308500072072400300641035700000602054"
-        grid = Grid.new puzzle
-        grid.solve
-        puts grid.print_in_terminal
-    end
 end
 
